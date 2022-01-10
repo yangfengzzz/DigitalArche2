db7a11e63e6b337478d1375f8985237d7f3c29a9
yangfeng <yangfengzzz@hotmail.com>
2022-01-10 08:35:29 +0800

    add metal cpp

---
+++ vox.render/core/impl.cpp
@@ -0,0 +1,13 @@
+//
+//  impl.cpp
+//  DigitalArche2
+//
+//  Created by 杨丰 on 2022/1/10.
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include "Foundation.hpp"
+#include "Metal.hpp"
+#include "QuartzCore.hpp"
