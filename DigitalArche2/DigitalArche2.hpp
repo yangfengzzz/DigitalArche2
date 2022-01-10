039640de8f71eea23894e67c8ffffd11c5bf79f0
yangfeng <yangfengzzz@hotmail.com>
2022-01-10 08:25:57 +0800

    Initial Commit

---
+++ DigitalArche2/DigitalArche2.hpp
@@ -0,0 +1,21 @@
+//
+//  DigitalArche2.hpp
+//  DigitalArche2
+//
+//  Created by 杨丰 on 2022/1/10.
+//
+
+#ifndef DigitalArche2_
+#define DigitalArche2_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class DigitalArche2
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ DigitalArche2/DigitalArche2Priv.hpp
@@ -0,0 +1,17 @@
+//
+//  DigitalArche2Priv.hpp
+//  DigitalArche2
+//
+//  Created by 杨丰 on 2022/1/10.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class DigitalArche2Priv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
